d1ffbdae640cbbfaea23edeab7484934c854d676
Annie Wång <annie.u.wang@gmail.com>
2022-04-17 18:37:44 +0200

    added home-page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Ping from '../components/Ping.vue';
+import Home from '../components/Home.vue'
 
 const routes = [
     {
         path: '/ping',
         name: 'Ping',
         component: Ping
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
     },
 ];
 
